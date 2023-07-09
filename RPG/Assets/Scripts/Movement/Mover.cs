2364419b0d3170e32bfbed36848acdac3d0528ac
Gustavo <gustavopiekala22@gmail.com>
2023-07-09 17:30:36 -0300

    init

---
+++ RPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,47 @@
+﻿using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        NavMeshAgent navMeshAgent;
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("fowardSpeed", speed);
+        }
+
+        public void StartMoveAction(Vector3 destination) 
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+        
+        private void Start() 
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        public void Cancel() 
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+    }
+}
